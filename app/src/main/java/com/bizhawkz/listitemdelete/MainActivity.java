69e50403af7a54ecf14cbb9339e0513c893239fe
Heena <heenanautiyalcse@gmail.com>
2017-05-06 10:17:50 +0530

    Remove the items from static list

--- app/src/main/java/com/bizhawkz/listitemdelete/MainActivity.java
+++ app/src/main/java/com/bizhawkz/listitemdelete/MainActivity.java
@@ -1,13 +1,59 @@
 package com.bizhawkz.listitemdelete;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends ListActivity {
+    ArrayList list = new ArrayList();
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btnAdd);
+        Button btnDel = (Button) findViewById(R.id.btnDel);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, list);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edit = (EditText) findViewById(R.id.txtItem);
+                list.add(edit.getText().toString());
+                edit.setText("");
+                adapter.notifyDataSetChanged();
+            }
+        };
+        View.OnClickListener listenerDel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SparseBooleanArray checkedItemPositions = getListView().getCheckedItemPositions();
+                int itemCount = getListView().getCount();
+
+                for(int i=itemCount-1; i >= 0; i--){
+                    if(checkedItemPositions.get(i)){
+                        adapter.remove(list.get(i));
+                    }
+                }
+                checkedItemPositions.clear();
+                adapter.notifyDataSetChanged();
+            }
+        };
+
+        /** Setting the event listener for the add button */
+        btn.setOnClickListener(listener);
+
+        /** Setting the event listener for the delete button */
+        btnDel.setOnClickListener(listenerDel);
+
+        /** Setting the adapter to the ListView */
+        setListAdapter(adapter);
+
     }
 }
